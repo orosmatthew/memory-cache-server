a1593b4a07b300b54b93aa4daa7e20316b7463b5
orosmatthew <orosmatthew@pm.me>
2022-11-22 15:54:28 -0500

    Implement store command

--- src/main.c
+++ src/main.c
@@ -7,16 +7,118 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdbool.h>
+#include <pthread.h>
 
 #define SERVER_PORT 1052 // we have 1050 range
 #define BUF_SIZE 256
+
+#define CACHE_SIZE 8
+#define FILENAME_SIZE 256
+#define FILE_CONTENT_SIZE 1024
+
+typedef struct _cache_entry
+{
+    char filename[FILENAME_SIZE];
+    int n_bytes;
+    char *p_contents;
+    bool is_valid;
+} CacheEntry;
+
+typedef struct _cache
+{
+    CacheEntry entries[CACHE_SIZE];
+} Cache;
+
+pthread_mutex_t cache_mutex = PTHREAD_MUTEX_INITIALIZER;
+Cache cache;
+
+void print_cache()
+{
+    pthread_mutex_lock(&cache_mutex);
+    printf("\nCACHE:\n");
+    for (int i = 0; i < CACHE_SIZE; i++)
+    {
+        if (cache.entries[i].is_valid)
+        {
+            printf("===================\n");
+            printf("FILENAME: %s\n", cache.entries[i].filename);
+            printf("BYTES: %d\n", cache.entries[i].n_bytes);
+            printf("CONTENTS: %s\n", cache.entries[i].p_contents);
+            printf("===================\n");
+        }
+    }
+    pthread_mutex_unlock(&cache_mutex);
+}
+
+int hash_filename(char *filename)
+{
+    return (int)(filename[0]);
+}
 
 void command_load(char *args)
 {
 }
 
-void command_store(char *args)
+void command_store(int arg_size, char *args)
 {
+    char filename[FILENAME_SIZE];
+    char n_bytes_str[8];
+    char *contents = (char *)malloc(FILE_CONTENT_SIZE);
+
+    int step = 0;   // 0: filename, 1: n_bytes_str, 2: contents
+    int offset = 0; // offset from last arg
+    for (int i = 0; i < arg_size + 1; i++)
+    {
+        // Parsing filename
+        if (step == 0 && args[i] != ' ')
+        {
+            filename[i] = args[i];
+        }
+        else if (step == 0)
+        {
+            filename[i] = '\0';
+            i++;
+            offset = i;
+            step++;
+        }
+
+        // Parsing n_bytes_str
+        if (step == 1 && args[i] != ':')
+        {
+            n_bytes_str[i - offset] = args[i];
+        }
+        else if (step == 1)
+        {
+            n_bytes_str[i - offset] = '\0';
+            i++;
+            offset = i;
+            step++;
+        }
+
+        // Parsing contents
+        if (step == 2)
+        {
+            contents[i - offset] = args[i];
+        }
+    }
+
+    CacheEntry entry;
+    entry.is_valid = true;
+    strcpy(entry.filename, filename);
+    entry.n_bytes = atoi(n_bytes_str);
+    entry.p_contents = contents;
+
+    int index = hash_filename(filename) % CACHE_SIZE;
+
+    pthread_mutex_lock(&cache_mutex);
+    if (cache.entries[index].is_valid)
+    {
+        free(cache.entries[index].p_contents);
+    }
+    cache.entries[index] = entry;
+    pthread_mutex_unlock(&cache_mutex);
+
+    print_cache();
 }
 
 void command_remove(char *args)
@@ -25,11 +127,13 @@ void command_remove(char *args)
 
 int main(int argc, char *argv[])
 {
+    memset(&cache, 0, sizeof(Cache));
+
     int serverSocket, bytesRead;
 
     // These are the buffers to talk back and forth with the server
     char sendLine[BUF_SIZE];
-    char receiveLine[BUF_SIZE] = "store filename n:contents";
+    char receiveLine[BUF_SIZE] = "store filename 9:qwertyuio";
 
     // // Create socket to server
     // if ((serverSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0)
@@ -79,7 +183,6 @@ int main(int argc, char *argv[])
         bool is_command = true;
         for (int i = 0; i < bytesRead; i++)
         {
-
             if (is_command)
             {
                 if (receiveLine[i] != ' ')
@@ -115,6 +218,7 @@ int main(int argc, char *argv[])
         if (strcmp(command_buffer, "store") == 0)
         {
             printf("STORE\n");
+            command_store(strlen(args_buffer), args_buffer);
         }
         if (strcmp(command_buffer, "rm") == 0)
         {
