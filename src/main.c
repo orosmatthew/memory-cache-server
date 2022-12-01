577f9d695b486ae6fd09ad35a8e74f817f0c1974
orosmatthew <orosmatthew@pm.me>
2022-12-01 12:26:31 -0500

    Add clang-format and reformat

--- src/main.c
+++ src/main.c
@@ -1,15 +1,14 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <pthread.h>
+#include <signal.h>
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <stdbool.h>
-#include <pthread.h>
-#include <signal.h>
-
 
 #define SERVER_PORT 1052 // we have 1050 range
 #define BUF_SIZE 256
@@ -18,16 +17,14 @@
 #define FILENAME_SIZE 256
 #define FILE_CONTENT_SIZE 1024
 
-typedef struct
-{
+typedef struct {
     char filename[FILENAME_SIZE];
     int n_bytes;
-    char* p_contents;
+    char *p_contents;
     bool is_valid;
 } CacheEntry;
 
-typedef struct
-{
+typedef struct {
     CacheEntry entries[CACHE_SIZE];
 } Cache;
 
@@ -37,15 +34,14 @@ Cache cache;
 void print_cache_specific(bool existsInCache, int entryIndex)
 {
     printf("\nCACHE:\n");
-    if (existsInCache == 1 && entryIndex < 8)
-    {
+    if (existsInCache == 1 && entryIndex < 8) {
         printf("===================\n");
         printf("FILENAME: %s\n", cache.entries[entryIndex].filename);
         printf("BYTES: %d\n", cache.entries[entryIndex].n_bytes);
         printf("CONTENTS: %s\n", cache.entries[entryIndex].p_contents);
         printf("===================\n");
-    } else
-    {
+    }
+    else {
         printf("===================\n");
         printf("FILENAME: FILE DOES NOT EXIST\n");
         printf("BYTES: 0");
@@ -57,10 +53,8 @@ void print_cache_specific(bool existsInCache, int entryIndex)
 void print_cache()
 {
     printf("\nCACHE:\n");
-    for (int i = 0; i < CACHE_SIZE; i++)
-    {
-        if (cache.entries[i].is_valid)
-        {
+    for (int i = 0; i < CACHE_SIZE; i++) {
+        if (cache.entries[i].is_valid) {
             printf("===================\n");
             printf("FILENAME: %s\n", cache.entries[i].filename);
             printf("BYTES: %d\n", cache.entries[i].n_bytes);
@@ -70,21 +64,19 @@ void print_cache()
     }
 }
 
-int hash_filename(char* filename)
+int hash_filename(char *filename)
 {
-    return (int) (filename[0]);
+    return (int)(filename[0]);
 }
 
-void command_load(size_t arg_size, const char* args)
+void command_load(size_t arg_size, const char *args)
 {
     char filename[FILENAME_SIZE];
-    for (int i = 0; i < arg_size + 1; i++)
-    {
-        if (args[i] != ' ')
-        {
+    for (int i = 0; i < arg_size + 1; i++) {
+        if (args[i] != ' ') {
             filename[i] = args[i];
-        } else
-        {
+        }
+        else {
             filename[i] = '\0';
         }
     }
@@ -92,34 +84,30 @@ void command_load(size_t arg_size, const char* args)
     int index = hash_filename(filename) % CACHE_SIZE;
 
     pthread_mutex_lock(&cache_mutex);
-    if (strcmp(cache.entries[index].filename, filename) == 0 && cache.entries[index].is_valid)
-    {
+    if (strcmp(cache.entries[index].filename, filename) == 0 && cache.entries[index].is_valid) {
         print_cache_specific(true, index);
-    } else if (strcmp(cache.entries[index].filename, filename) != 0 || !cache.entries[index].is_valid)
-    {
+    }
+    else if (strcmp(cache.entries[index].filename, filename) != 0 || !cache.entries[index].is_valid) {
         // just using 0 here as a null since we don't use the second parameter in this scenario anyway
-        print_cache_specific(false,
-                             0);
+        print_cache_specific(false, 0);
     }
     pthread_mutex_unlock(&cache_mutex);
 }
 
-void command_store(size_t arg_size, const char* args)
+void command_store(size_t arg_size, const char *args)
 {
     char filename[FILENAME_SIZE];
     char n_bytes_str[8];
-    char* contents = (char*) malloc(FILE_CONTENT_SIZE);
+    char *contents = (char *)malloc(FILE_CONTENT_SIZE);
 
-    int step = 0;   // 0: filename, 1: n_bytes_str, 2: contents
+    int step = 0; // 0: filename, 1: n_bytes_str, 2: contents
     int offset = 0; // offset from last arg
-    for (int i = 0; i < arg_size + 1; i++)
-    {
+    for (int i = 0; i < arg_size + 1; i++) {
         // Parsing filename
-        if (step == 0 && args[i] != ' ')
-        {
+        if (step == 0 && args[i] != ' ') {
             filename[i] = args[i];
-        } else if (step == 0)
-        {
+        }
+        else if (step == 0) {
             filename[i] = '\0';
             i++;
             offset = i;
@@ -127,11 +115,10 @@ void command_store(size_t arg_size, const char* args)
         }
 
         // Parsing n_bytes_str
-        if (step == 1 && args[i] != ':')
-        {
+        if (step == 1 && args[i] != ':') {
             n_bytes_str[i - offset] = args[i];
-        } else if (step == 1)
-        {
+        }
+        else if (step == 1) {
             n_bytes_str[i - offset] = '\0';
             i++;
             offset = i;
@@ -139,8 +126,7 @@ void command_store(size_t arg_size, const char* args)
         }
 
         // Parsing contents
-        if (step == 2)
-        {
+        if (step == 2) {
             contents[i - offset] = args[i];
         }
     }
@@ -154,34 +140,31 @@ void command_store(size_t arg_size, const char* args)
     int index = hash_filename(filename) % CACHE_SIZE;
 
     pthread_mutex_lock(&cache_mutex);
-    if (cache.entries[index].is_valid)
-    {
+    if (cache.entries[index].is_valid) {
         free(cache.entries[index].p_contents);
     }
     cache.entries[index] = entry;
     pthread_mutex_unlock(&cache_mutex);
 }
 
-void command_remove(size_t arg_size, const char* args)
+void command_remove(size_t arg_size, const char *args)
 {
     char filename[FILENAME_SIZE];
-    for (int i = 0; i < FILENAME_SIZE || i < arg_size; i++)
-    {
+    for (int i = 0; i < FILENAME_SIZE || i < arg_size; i++) {
         filename[i] = args[i];
     }
 
     int index = hash_filename(filename) % CACHE_SIZE;
 
     pthread_mutex_lock(&cache_mutex);
-    if (strcmp(cache.entries[index].filename, filename) == 0 && cache.entries[index].is_valid)
-    {
+    if (strcmp(cache.entries[index].filename, filename) == 0 && cache.entries[index].is_valid) {
         cache.entries[index].is_valid = false;
         printf("===================\n");
         printf("SUCCESSFULLY REMOVED FILE: %s\n", cache.entries[index].filename);
         printf("===================\n");
-    } else if ((strcmp(cache.entries[index].filename, filename) != 0 ||
-                ((strcmp(cache.entries[index].filename, filename) == 0 && cache.entries[index].is_valid == false))))
-    {
+    }
+    else if ((strcmp(cache.entries[index].filename, filename) != 0
+              || ((strcmp(cache.entries[index].filename, filename) == 0 && cache.entries[index].is_valid == false)))) {
         printf("===================\n");
         printf("SORRY BUT THE FILE YOU ARE ATTEMPTING TO REMOVE DOES NOT EXIST.\n");
         printf("PLEASE CHECK SPELLING.\n");
@@ -191,7 +174,7 @@ void command_remove(size_t arg_size, const char* args)
     pthread_mutex_unlock(&cache_mutex);
 }
 
-void process_input(size_t input_size, const char* input)
+void process_input(size_t input_size, const char *input)
 {
     // receiveLine[bytesRead] = 0; // Make sure we put the null terminator at the end of the buffer
     // printf("Received %d bytes from server with message: %s\n", bytesRead, receiveLine);
@@ -200,42 +183,35 @@ void process_input(size_t input_size, const char* input)
     int command_size = 0; // NOTE: Might be off by 1
     char args_buffer[100];
     bool is_command = true;
-    for (int i = 0; i < input_size; i++)
-    {
-        if (is_command)
-        {
-            if (input[i] != ' ')
-            {
+    for (int i = 0; i < input_size; i++) {
+        if (is_command) {
+            if (input[i] != ' ') {
                 command_buffer[i] = input[i];
-            } else
-            {
+            }
+            else {
                 command_buffer[i] = '\0';
                 is_command = false;
                 command_size = i;
             }
-        } else
-        {
-            if (input[i] != '\0')
-            {
+        }
+        else {
+            if (input[i] != '\0') {
                 args_buffer[i - command_size - 1] = input[i];
-            } else
-            {
+            }
+            else {
                 args_buffer[i - command_size - 1] = '\0';
                 break;
             }
         }
     }
 
-    if (strcmp(command_buffer, "load") == 0)
-    {
+    if (strcmp(command_buffer, "load") == 0) {
         command_load(strlen(args_buffer), args_buffer);
     }
-    if (strcmp(command_buffer, "store") == 0)
-    {
+    if (strcmp(command_buffer, "store") == 0) {
         command_store(strlen(args_buffer), args_buffer);
     }
-    if (strcmp(command_buffer, "rm") == 0)
-    {
+    if (strcmp(command_buffer, "rm") == 0) {
         command_remove(strlen(args_buffer), args_buffer);
     }
 }
@@ -250,15 +226,13 @@ void closeConnection()
     exit(1);
 }
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     memset(&cache, 0, sizeof(Cache));
-
 
     // These are the buffers to talk back and forth with the server
     char sendLine[BUF_SIZE];
     char receiveLine[BUF_SIZE];
-
 
     int connectionToClient;
     size_t bytesRead;
@@ -273,10 +247,8 @@ int main(int argc, char* argv[])
     serverAddress.sin_addr.s_addr = htonl(INADDR_ANY);
     serverAddress.sin_port = htons(SERVER_PORT);
 
-
     // Bind to port, error message if failure to bind
-    if (bind(serverSocket, (struct sockaddr*) &serverAddress, sizeof(serverAddress)) == -1)
-    {
+    if (bind(serverSocket, (struct sockaddr *)&serverAddress, sizeof(serverAddress)) == -1) {
         printf("Unable to bind to port\n");
         exit(-1);
     }
@@ -287,26 +259,21 @@ int main(int argc, char* argv[])
     sigIntHandler.sa_flags = 0;
     sigaction(SIGINT, &sigIntHandler, NULL);
 
-
     // Start listening for up to 10 connections
     listen(serverSocket, 10);
 
-    while (true)
-    {
-        connectionToClient = accept(serverSocket, (struct sockaddr*) NULL, NULL);
+    while (true) {
+        connectionToClient = accept(serverSocket, (struct sockaddr *)NULL, NULL);
 
         // Read command from client
-        while ((bytesRead = read(connectionToClient, receiveLine, BUF_SIZE)) > 0)
-        {
+        while ((bytesRead = read(connectionToClient, receiveLine, BUF_SIZE)) > 0) {
             // Put NULL terminator at end
             receiveLine[bytesRead] = 0;
 
             // Start thread to handle command input
             pthread_t processThread;
-            //pthread_create(&processThread, NULL, process_input, (void *))
-            // Need to change process_input to allow for threading
-
+            // pthread_create(&processThread, NULL, process_input, (void *))
+            //  Need to change process_input to allow for threading
         }
     }
-
 }
